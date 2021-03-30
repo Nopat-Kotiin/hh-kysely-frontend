bcd8f0186f0a579fdb98e9275ca6bdd5f5bf70e3
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-03-30 14:23:05 +0300

    init

---
+++ hh-kysely-backend/src/main/java/fi/hh/swd22/HHkysely/HhKyselyApplication.java
@@ -0,0 +1,13 @@
+package fi.hh.swd22.HHkysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhKyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhKyselyApplication.class, args);
+	}
+
+}
---
+++ hh-kysely-backend/src/test/java/fi/hh/swd22/HHkysely/HhKyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.hh.swd22.HHkysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhKyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
