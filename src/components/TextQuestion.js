3d1f859df632d13a7b05b1dbc1228cead5fe51ad
jontsaaw <64011152+jontsaaw@users.noreply.github.com>
2021-04-29 10:34:33 +0300

    added radio,checkbox and text components

--- src/components/TextQuestion.js
+++ src/components/TextQuestion.js
@@ -2,22 +2,20 @@ import { useEffect, useState } from "react";
 import TextAreaAutosize from 'react-textarea-autosize';
 
 function Question(props) {
-  const [question, setQuestion] = useState(props.question);
+  const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
 
   useEffect(() => {
-    setQuestion({...question, answers: [{answer: answer}]});
-    props.update(question, props.index);
-  }, [answer]);
+    setQuestion(props.name);
+  }, []);
 
   const inputChanged = (event) => {
     setAnswer(event.target.value);
-    setQuestion({...question, answers: [{answer: event.target.value}]});
   }
 
   return (
     <div>
-      <h3 key={question.id}>{question.question}</h3>
+      <h3>{question}</h3>
 
       <TextAreaAutosize
         onChange={inputChanged}
