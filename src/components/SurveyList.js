72ce598dd23b2e0734accf684ed68a5046ae81fb
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-05-18 15:37:41 +0300

    css

--- src/components/SurveyList.js
+++ src/components/SurveyList.js
@@ -5,7 +5,8 @@ function SurveyList(props) {
       {props.data.map((row, index) => {
           return (
             <div key={index}>
-              <a href={"/survey/" + row.id}><h2 key={row.id} >{row.name}</h2></a>
+              <h2 key={row.id} >{row.name}</h2>
+              <a href={"/survey/" + row.id}><button>Vastaa</button></a>
               <a href={"/surveyanswers/" + row.id}><button>Katso vastaukset</button></a>
               <a href={"/surveystatistics/" + row.id}><button>Katso statistiikkaa</button></a>
             </div>
